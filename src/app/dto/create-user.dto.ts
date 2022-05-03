9602fceb22279bafa2a954557d192be96d0345eb
blamet <arturbaldoramos@gmail.com>
2022-05-03 17:46:33 -0300

    update user reques parameters

--- src/app/dto/create-user.dto.ts
+++ src/app/dto/create-user.dto.ts
@@ -1,9 +1,17 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNotIn, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { UserService } from "../user.service";
 
 export class CreateUserDto {
     @IsNotEmpty()
+    @IsString()
     username: string;
 
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+      message: 'password too weak',
+    })
     password: string;
 }
